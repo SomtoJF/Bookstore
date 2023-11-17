ffe8a7c9bbe1a1ce4bd0f99e7ba16a396b91e79c
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-17 23:49:00 +0100

    Set up routing in the frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Createbook from "../pages/Createbook";
+import Showbook from "../pages/Showbook";
+import Editbook from "../pages/Editbook";
+import Deletebook from "../pages/Deletebook";
 
 export default function App() {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/books/create" element={<Createbook />} />
+				<Route path="/books/details/:id" element={<Showbook />} />
+				<Route path="/books/edit/:id" element={<Editbook />} />
+				<Route path="/books/delete/:id" element={<Deletebook />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
