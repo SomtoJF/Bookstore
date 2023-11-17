e7eb78bea90939e1d02ccc4d714539cb097fde21
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-17 21:27:42 +0100

    Connect to mongoDB database

--- server/app.js
+++ server/app.js
@@ -5,6 +5,24 @@ require("dotenv").config();
 
 app.use(express.json());
 
+const mongoose = require("mongoose");
+
+async function connectDB() {
+	try {
+		console.log("connecting...");
+		await mongoose.connect(process.env.DATABASE_URL);
+		console.log("Connection established");
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+connectDB();
+
+app.get("/", (req, res) => {
+	res.send("hello");
+});
+
 app.listen(PORT, () => {
 	console.log("listening on port " + PORT);
 });
