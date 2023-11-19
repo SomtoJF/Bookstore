ef87a16265811b3b8aa90339a480a14237c29242
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-11-19 13:55:37 +0100

    create book service calls wrong API endpoint

--- client/src/services/books/createBook.ts
+++ client/src/services/books/createBook.ts
@@ -4,7 +4,7 @@ import { BACKEND_URL } from "../../../config";
 
 export default async function createBook(payload: CreateBookPayload) {
 	try {
-		const response = await axios.post(BACKEND_URL, payload);
+		const response = await axios.post(BACKEND_URL + "books", payload);
 		return response.data;
 	} catch (err: any) {
 		throw new Error(err);
